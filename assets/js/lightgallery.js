9247a2be5f61362733f88d7e1c2b7eeeb13087fa
Dan Gilmore <2inchesofwater@users.noreply.github.com>
2019-05-06 07:51:50 +1000

    Bug: Hover state for gallery items
    Fixed the z-index on the border around individual gallery items in Kirby, as it was preventing the gallery item responding to hover events
    
    Also fixed up a bug in the assertive accessible text in the counter on the Lightgallery toolbar.

--- assets/js/lightgallery.js
+++ assets/js/lightgallery.js
@@ -655,7 +655,7 @@
      */
     Plugin.prototype.counter = function () {
         if (this.s.counter) {
-            this.outer.querySelector(this.s.appendCounterTo).insertAdjacentHTML('beforeend', '<div id="lg-counter" id="lg-counter-current">' + (parseInt(this.index, 10) + 1) + '</span> / <span id="lg-counter-all">' + this.items.length + '</span></div>');
+            this.outer.querySelector(this.s.appendCounterTo).insertAdjacentHTML('beforeend', '<div id="lg-counter" class="sr-only sr-assertive" aria-live="assertive"><span id="lg-counter-current">' + (parseInt(this.index, 10) + 1) + '</span> / <span id="lg-counter-all">' + this.items.length + '</span></div>');
         }
     };
 
